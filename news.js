9c7084a247ab76e76d3a7c81412ae1fec4cca63c
Dorian Gruselin <dorian.gruselin@hotmail.com>
2016-05-24 17:02:17 +0200

    Corrections + amélioration des actualités
    - Correction de TOUS les "Tout droit réservé" en "Tous droits réservés"
    - Modification du Jquery "news.js" pour que la taille de ".clic"
    s'adapte à la taille de ".article" lorsque l'article est ouvert
    - Ajout d'un article d'exemple
    - Modification de la police d'écriture du titre + titre découpé si il
    doit aller à la ligne (à améliorer)

--- news.js
+++ news.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$(".titre").click(function() {
 		var i = $(this).parent().parent();
+		var j = $(this).parent().height();
 		if ($(i).css("height") == "80px") {
-		$(i).animate({height:"800px"}, 500);
+		$(i).animate({height:j+"px"}, 500);
 		} else {
 			$(i).animate({height:"80px"}, 500);
 		}
