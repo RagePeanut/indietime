a2971c72ab9ef46a57acd6ccc39a2cbbf9605a62
Dorian Gruselin <dorian.gruselin@hotmail.com>
2016-05-28 20:41:39 +0200

    Transitions et bas d'article
    - Ajout de transitions sur Hall of Shame/Fame
    - Ajout de bas d'article avec nom d'auteur + photo de profile + citation
    de l'auteur
    - (Tests non concluants pour améliorer le script news.js)

--- news.js
+++ news.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function taille() {
 	$(".titre").click(function() {
+		var test = true;
 		var i = $(this).parent().parent();
-		var j = $(this).parent().height();
 		if ($(i).css("height") == "80px") {
-		$(i).animate({height:j+"px"}, 500);
+				var j = $(this).parent().height();
+				$(i).animate({height:j+"px"}, 500);
 		} else {
 			$(i).animate({height:"80px"}, 500);
-		}
+		};
 	});
 });
