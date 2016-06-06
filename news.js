7d490fc07cdbfd8c0ea9c89aab902d8efde505d5
Dorian Gruselin <dorian.gruselin@hotmail.com>
2016-06-06 21:48:48 +0200

    Ajout d'article + modifications minimes de design
    - Ajout de lien entre les articles de l'index et de l'onglet actualités

--- news.js
+++ news.js
@@ -1,6 +1,5 @@
 $(document).ready(function taille() {
 	$(".titre").click(function() {
-		var test = true;
 		var i = $(this).parent().parent();
 		if ($(i).css("height") == "80px") {
 				var j = $(this).parent().height();
